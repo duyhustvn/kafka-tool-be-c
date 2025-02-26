821965b80fe2c36d2ee2cd141603e30c7b75f8bc
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2025-02-26 22:06:49 +0700

    parse http request line

--- src/http_request_line.c
+++ src/http_request_line.c
@@ -1,14 +1,15 @@
 #include "http_request_line.h"
 
 #include "queue.h"
+#include <stdio.h>
 
-http_request_line* parse_http_request_line(char *request_line, int n) {
+http_request_line* parse_http_request_line(char *request_line, int len) {
     int count = 0;
     int start = 0;
 
-   queue *q = init_queue();
+    queue *q = init_queue();
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < len; i++) {
         char c = request_line[i];
         if  (c == ' ') {
             char item[count];
@@ -20,7 +21,7 @@ http_request_line* parse_http_request_line(char *request_line, int n) {
             continue;
         }
 
-        else if (i == n-1) {
+        else if (i == len-1) {
             char item[count+1];
             strncpy(item, request_line + start, count+1);
             start = i+1;
@@ -32,9 +33,22 @@ http_request_line* parse_http_request_line(char *request_line, int n) {
         count++;
     }
 
+
+
     if (q->size != 3) {
+#ifdef DEBUG
+        printf("queue size: %d\n", q->size);
+        printf("len: %d\n", len);
+        printf("request_line: %s\n", request_line);
+        int qs = q->size;
+        for (int i = 0; i < qs; i++) {
+            node* nd = dequeue(q);
+            printf("node value: %s\n", nd->value);
+        }
+#endif
         return NULL;
     }
+
 
     http_request_line *request = malloc(sizeof(http_request_line));
     node* method = dequeue(q);
--- test/test_http_request_line.c
+++ test/test_http_request_line.c
@@ -1,6 +1,8 @@
 #include "unity.h"
 
 #include "http_request_line.h"
+#include "queue.h"
+#include <string.h>
 
 
 typedef struct Test_ {
@@ -23,14 +25,26 @@ void test_parse_http_request_line() {
     .input="GET /test1 HTTP/1.1",
     .expected={.method="GET",.path="/test1",.protocol="HTTP/1.1"},
     },
+    {
+    .input="POST /api/v1/test HTTP/1.1",
+    .expected={.method="POST",.path="/api/v1/test",.protocol="HTTP/1.1"},
+    },
+    {
+    .input="DELETE / HTTP/1.1",
+    .expected={.method="DELETE",.path="/",.protocol="HTTP/1.1"},
+    },
     };
 
     int n = sizeof(tests)/sizeof(tests[0]);
     for (int i = 0; i < n; i++) {
         Test test = tests[i];
 
-        http_request_line *request_line = parse_http_request_line(test.input, sizeof(test.input));
+        http_request_line *request_line = parse_http_request_line(test.input, strlen(test.input));
+        TEST_ASSERT_NOT_NULL(request_line);
         TEST_ASSERT_EQUAL_STRING(test.expected.method, request_line->method);
+        TEST_ASSERT_EQUAL_STRING(test.expected.path, request_line->path);
+        TEST_ASSERT_EQUAL_STRING(test.expected.protocol, request_line->protocol);
+
         free(request_line);
     }
 }
